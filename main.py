2bdfdf596a7c03030914c7b4f4fa0aeab94e1780
yugen <yugoso8512@gmail.com>
2024-02-24 22:01:25 -0500

    se añaden dependencias y se atajan los datos del form

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from typing import Annotated
 
 app = FastAPI()
 
@@ -12,6 +13,13 @@ def root(request: Request):
     return jinja2_templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get("/dashboard", response_class=HTMLResponse)
+@app.get("/users/dashboard", response_class=HTMLResponse)
 def dashboard(request: Request):
     return jinja2_templates.TemplateResponse("dashboard.html", {"request": request})
+
+@app.post("/users/login", )
+def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return {
+        "username": username,
+        "password": password
+    }
